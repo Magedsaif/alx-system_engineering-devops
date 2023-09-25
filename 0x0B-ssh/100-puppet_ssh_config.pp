e6427155b1adca2e06c677e39779aca8411af51c
magedsaif <magedsaif21@gmail.com>
2023-09-25 20:48:26 +0200

    edit task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,9 +3,11 @@
 #this script is to be run on the puppet master
 file { '/etc/ssh/ssh_config':
         ensure => present,
-content => '
+content => "
+Include /etc/ssh/ssh_config.d/*.conf
 Host *
     IdentityFile ~/.ssh/school
     passwordAuthentication no
-    '
+",
+  mode    => '0644',
 }
