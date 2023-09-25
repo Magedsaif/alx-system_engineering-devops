8ebce011696f8a54b4074bea3e69ad7f1aa7db36
magedsaif <magedsaif21@gmail.com>
2023-09-25 20:44:21 +0200

    task 100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#!usr/bin/env bash
+#use puppet to make changes to my config files
+#this script is to be run on the puppet master
+file { '/etc/ssh/ssh_config':
+        ensure => present,
+content => '
+Host *
+    IdentityFile ~/.ssh/school
+    passwordAuthentication no
+    '
+}
