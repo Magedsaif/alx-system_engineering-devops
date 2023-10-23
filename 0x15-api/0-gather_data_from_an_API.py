7d279e366019234ec66f1f2a383591391402eb54
magedsaif <magedsaif21@gmail.com>
2023-10-23 18:48:11 +0200

    edit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,42 @@
 #!/usr/bin/python3
-"""gets api"""
+"""A Python script that, using this REST API, for a given employee ID."""
 import requests
 from sys import argv
 
 
-def todo(userid):
-    """doc stringed"""
-    name = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(
-            userid)).json().get('name')
-    tasks = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
-            userid)).json()
-    tasksDone = ['\t {}\n'.format(dic.get('title')) for dic in tasks
-                 if dic.get('completed')]
-    if name and tasks:
-        print("Employee {} is done with tasks({}/{}):".format
-              (name, len(tasksDone), len(tasks)))
-        print(''.join(tasksDone), end='')
+def display_tasks():
+    """Python script that fetches data for a given employee ID."""
+    # link to fetch data from the API
+    link = "https://jsonplaceholder.typicode.com/users/"
+
+    # get the data from
+    response = requests.get(link + argv[1])
+
+    # convert the data to json format
+    data = response.json()
+
+    # get the name of the employee
+    name = data.get("name")
+    print("Employee {} is done with tasks".format(name), end="")
+
+    # get the total number of tasks of the employee
+    response = requests.get(link + argv[1] + "/todos")
+
+    # convert the data to json format
+    data = response.json()
+
+    # get the completed tasks and the total number of them
+    tasks = []
+    for task in data:
+        if task.get("completed") is True:
+            tasks.append(task.get("title"))
+    print("({}/{}):".format(len(tasks), len(data)))
+
+    # print the completed tasks of the employee
+    for task in tasks:
+        formatted_task = task.replace(" ", "S").replace("\t", "T")
+        print("\t {}".format(formatted_task))
 
 
 if __name__ == "__main__":
-    if len(argv) == 2:
-        todo(int(argv[1]))
+    display_tasks()
