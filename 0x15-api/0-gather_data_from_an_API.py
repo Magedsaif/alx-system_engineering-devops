564041c37580bc03d27b3f880e97c824e9d7a253
magedsaif <magedsaif21@gmail.com>
2023-10-23 14:46:07 +0200

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""
-A Python script that, using this REST API, for a given employee ID.
-
-returns information about his/her TODO list progress.
-"""
-
+"""A Python script that, using this REST API, for a given employee ID."""
 import requests
 from sys import argv
 
 # link to fetch data from the API
 link = "https://jsonplaceholder.typicode.com/users/"
 
-# get the data from the API
+# get the data from
 response = requests.get(link + argv[1])
 
 # convert the data to json format
@@ -37,3 +32,6 @@ print("({}/{}):".format(len(tasks), len(data)))
 # print the completed tasks
 for task in tasks:
     print("\t {}".format(task))
+
+if __name__ == "__main__":
+    pass
