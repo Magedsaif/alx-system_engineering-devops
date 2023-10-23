d1464e9859acd2cf8da6bc02aa6a465ebd6feb6b
magedsaif <magedsaif21@gmail.com>
2023-10-23 19:16:48 +0200

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,46 +1,29 @@
 #!/usr/bin/python3
-"""A Python script that, using this REST API, for a given employee ID."""
+"""A Python script using this REST API, for a given employee ID."""
 import csv
-import requests
+from requests import get
 from sys import argv
 
 
-def display_tasks():
-    """Python script that fetches data for a given employee ID."""
-    # link to fetch data from the API
-    link = "https://jsonplaceholder.typicode.com/users/"
-
-    # get the data from
-    response = requests.get(link + argv[1])
-
-    # convert the data to json format
-    data = response.json()
-
-    # get the name of the employee
-    name = data.get("name")
-    print("Employee {} is done with tasks".format(name), end="")
-
-    # get the total number of tasks of the employee
-    response = requests.get(link + argv[1] + "/todos")
-
-    # convert the data to json format
-    data = response.json()
-
-    # export as csv file named USER_ID.csv (ex: 2.csv)
-    # with columns: "USER_ID", "USERNAME", "TASK_COMPLETED_STATUS","TASK_TITLE"
-    with open("{}.csv".format(argv[1]), "w", newline="") as csvfile:
-        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
-                      "TASK_TITLE"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for task in data:
-            writer.writerow({
-                "USER_ID": argv[1],
-                "USERNAME": name,
-                "TASK_COMPLETED_STATUS": task.get("completed"),
-                "TASK_TITLE": task.get("title")
-            })
+def cvsWrite(user):
+    """Write to csv."""
+    # get the Response from the API endpoint and convert to json format
+    Response = get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                   .format(user)).json()
+    # get the employee name from the API endpoint and convert to json format
+    employee_name = get('https://jsonplaceholder.typicode.com/users/{}'.format(
+        user)).json().get('username')
+    # write the data to csv format and save to a file named USER_ID.csv
+    employ_data = open('{}.csv'.format(user), 'w')
+    # create the csv writer_object and write the data to the file in csv format
+    writer_object = csv.writer(employ_data, quoting=csv.QUOTE_ALL)
+    # write the data to the file in csv format
+    for line in Response:
+        lines = [line.get('userId'), employee_name,
+                 line.get('completed'), line.get('title')]
+        writer_object.writerow(lines)
+    employ_data.close()
 
 
 if __name__ == "__main__":
-    display_tasks()
+    cvsWrite(argv[1])
