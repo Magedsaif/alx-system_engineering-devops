b4de800794d79df04a7b8aff0202633d3540c093
Maged Saif <magedsaif21@gmail.com>
2023-11-07 21:06:00 +0200

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,4 +2,13 @@
 """Module to query the Reddit API and returns the number of subscribers"""
 import requests
 
-def top_ten(subreddit)
+def top_ten(subreddit):
+    """Returns the number of subscribers"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        for i in range(10):
+            print(response.json().get("data").get("children")[i].get("data").get("title"))
+    else:
+        print(None)
