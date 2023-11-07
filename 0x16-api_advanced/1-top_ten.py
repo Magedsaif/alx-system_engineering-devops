e7adeca4d7d72c66301563102e364ddf33fafdab
Maged Saif <magedsaif21@gmail.com>
2023-11-07 21:24:30 +0200

    edit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,23 +3,14 @@
 import requests
 
 
-def top_ten(subreddit: str) -> None:
-    """
-    Retrieves the top ten posts from a specified subreddit on Reddit and prints
-    their titles.
-
-    Args:
-        subreddit (str): The name of the subreddit to retrieve the top posts.
-
-    Returns:
-        None
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+def top_ten(subreddit):
+    """Returns the number of subscribers"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        posts = response.json().get("data", {}).get("children", [])
-        for post in posts[:10]:
-            print(post.get("data", {}).get("title"))
+        for i in range(10):
+            title = response.json()["data"]["children"][i]["data"]["title"]
+            print(title)
     else:
         print(None)
