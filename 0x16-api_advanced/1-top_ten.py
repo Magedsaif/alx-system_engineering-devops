09a893b72ace897aba8488bb0d023c7d466342e2
Maged Saif <magedsaif21@gmail.com>
2023-11-07 21:13:57 +0200

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,13 +2,24 @@
 """Module to query the Reddit API and returns the number of subscribers"""
 import requests
 
-def top_ten(subreddit):
-    """Returns the number of subscribers"""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+def top_ten(subreddit: str) -> None:
+    """
+    Retrieves the top ten posts from a specified subreddit on Reddit and prints
+    their titles.
+
+    Args:
+        subreddit (str): The name of the subreddit to retrieve the top posts.
+
+    Returns:
+        None
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        for i in range(10):
-            print(response.json().get("data").get("children")[i].get("data").get("title"))
+        posts = response.json().get("data", {}).get("children", [])
+        for post in posts[:10]:
+            print(post.get("data", {}).get("title"))
     else:
         print(None)
