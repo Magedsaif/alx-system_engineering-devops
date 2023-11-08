76ac0454f673861abf6000ba07a1db056d12978b
Maged Saif <magedsaif21@gmail.com>
2023-11-08 13:11:07 +0200

    task0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# fixes a file containing error
+
+exec {'replaces wrong php filetype':
+  command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+  onlyif  => 'test -f /var/www/html/wp-settings.php'
+}
